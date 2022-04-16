241bcc2cdd56eab636251036c0f5630903f76fd0
Ali Sbane <ali.sbane.dev@gmail.com>
2022-04-16 14:37:07 +0000

    Basic structure of the project Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import CardImage from "./components/CardImage";
+import PersonalInfo from "./components/PersonalInfo";
+import ContactMeButtons from "./components/ContactMeButtons";
+import About from "./components/About";
+import Interests from "./components/Interests";
+import Footer from "./components/Footer";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CardImage />
+      <PersonalInfo />
+      <ContactMeButtons />
+      <About />
+      <Interests />
+      <Footer />
+    </>
   );
 }
-
-export default App;
